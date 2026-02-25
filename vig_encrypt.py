8a1ccb616ed026dfa7f3d4516358d34530109041
Daniel Bostrom <119975555+dbos23@users.noreply.github.com>
2026-02-25 11:39:22 -0500

    Initial setup

---
+++ vig_encrypt.py
@@ -0,0 +1,29 @@
+def vig_encrypt(message, key):
+    '''Encrypts plaintext using a Vigenere cipher with a given key.'''
+
+    alph_l = 'abcdefghijklmnopqrstuvwxyz'
+    alph_u = alph_l.upper()
+    key = key.lower()
+    full_key = key
+
+    while len(message) > len(full_key):
+        full_key += key
+
+    if len(full_key) > len(message):
+        diff = len(full_key) - len(message)
+        full_key = full_key[:len(full_key) - diff]
+
+    l = []
+    count = 0
+    for i in range(len(message)):
+        l.append(message[i])
+        if l[i].isalpha():
+            if l[i].islower():
+                l[i] = alph_l[(alph_l.index(message[i]) + alph_l.index(full_key[count])) % 26]
+            else:
+                l[i] = alph_u[(alph_u.index(message[i]) + alph_u.index(full_key[count].upper())) % 26]
+            count += 1
+
+    return ''.join(l)
+
+print(vig_encrypt(input('Input message:\n'), input('Input key:\n')))
